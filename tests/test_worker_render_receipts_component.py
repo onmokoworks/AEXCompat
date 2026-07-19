8e7cd44ed0be80a2f71e50d51483e065a5e6511b
onmokoworks <onmokoworks@gmail.com>
2026-07-19 14:08:56 +0900

    test: follow item render runtime ownership

--- tests/test_worker_render_receipts_component.py
+++ tests/test_worker_render_receipts_component.py
@@ -15,6 +15,7 @@ REGISTRATION_SOURCES = MAIN + "\n" + "\n".join(
         "worker_aegp_external_render_runtime.cpp",
         "worker_aegp_async_layer_runtime.cpp",
         "worker_aegp_layer_render_runtime.cpp",
+        "worker_aegp_item_render_runtime.cpp",
     )
 )
 
